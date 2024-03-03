3bd49885edc0b55c1ab4d44caadd568d3bd3885b
BrianDeanO <brianrobertdean@gmail.com>
2024-03-02 16:06:23 -0800

    Cleaned up Graph component

--- hecate-app/src/HecateApp/Graph.tsx
+++ hecate-app/src/HecateApp/Graph.tsx
@@ -2,28 +2,8 @@ import React, { useEffect, useState, useCallback } from "react";
 import { SensorDataType } from "../interfaces";
 import { proxyURL, timeFrameConstants, monthsOfTheYear, repeatCountLimits } from "../variables.js";
 import axios from "axios";
-// import {
-//     Chart,
-//     CategoryScale,
-//     LinearScale,
-//     PointElement,
-//     LineElement,
-//     Title,
-//     Tooltip,
-//     Legend,
-//     defaults,
-//     TitleOptions,
-//     LineOptions
-// } from 'chart.js';
 import {Chart} from 'chart.js/auto'
 import { Line } from 'react-chartjs-2';
-
-// Chart.register(CategoryScale);
-// Chart.register(linear);
-// Chart.register(CategoryScale);
-// Chart.register(CategoryScale);
-// Chart.register(CategoryScale);
-// Chart.register(CategoryScale);
 
 interface GraphProps {
     selectedSensorID: number;
@@ -35,46 +15,32 @@ interface GraphProps {
 Chart.defaults.maintainAspectRatio = false;
 Chart.defaults.responsive = true;
 
-// defaults.plugins.title.display = true;
-// defaults.plugins.title.align =  "start";
-// // defaults.plugins.title.font = 20;
-// defaults.plugins.title.color = "black";
-
-
 const Graph: React.FC<GraphProps> = ({
     selectedSensorID,
     selectedChannelID,
     selectedTimeFrame,
     isLoggingOut
 }: GraphProps) => {
-    const [allSensorData, setAllSensorData] = useState([] as SensorDataType[]);
     const [sensorData, setSensorData] = useState([] as SensorDataType[]);
-    const [dataValues, setDataValues] = useState([] as number[]);
-    const [dataTimes, setDataTimes] = useState([] as string[]);
-    // const [currentTime, setCurrentTime] = useState(Date.now());
     const [timeLabels, setTimeLabels] = useState([] as string[])
 
     const getSensorData = useCallback(async(selectedSensorID: number) => {
         let tempAllSensorData: SensorDataType[] = [];
         let tempSensorData: SensorDataType[] = [];
         let tempTimeLabels: string[] = [];
-        let timeLabelCounts: number[] = [];
         let repeatCount = 0;
         const currentTime = Date.now();
 
-        console.log('getting sensor data', selectedTimeFrame)
         if(selectedSensorID !== 0 && !isLoggingOut) {
             await axios({
                 method: 'get',
                 url: `${proxyURL}/api/Sensor/${selectedSensorID}/SensorDatas`,
             })
-                .then(function (response) {
-                    tempAllSensorData = response.data;
-                }).catch(error => {
-                    console.log(error);
-                })
-                
-            // setAllSensorData(tempAllSensorData);
+            .then(function (response) {
+                tempAllSensorData = response.data;
+            }).catch(error => {
+                console.log(error);
+            })
 
             if((((selectedSensorID !== 0) && selectedSensorID !== undefined) ||
                 ((selectedChannelID !== 0) && selectedChannelID !== undefined))) {
@@ -86,28 +52,21 @@ const Graph: React.FC<GraphProps> = ({
                             tempSensorData.push(data);
                         }
                     })
-                
                     setSensorData(tempSensorData);
 
                     tempSensorData.forEach((data) => {
                         let timeLabel: string;
                         let repeatCountLimit: number;
+
                         if(data.channelID === selectedChannelID) {
-                            // console.log('selectedChannelID', selectedChannelID)
-                            // console.log('time recorded',  data.timeRecorded);
                             const tempFullDate = new Date(data.timeRecorded);
-                            // console.log('tempFullDate', tempFullDate);
+                            let labelRecorded = false;
+
                             let tempSeconds: string = tempFullDate.getSeconds().toString();
                             let tempMinutes: string = tempFullDate.getMinutes().toString();
                             let tempHours: string = tempFullDate.getHours().toString();
                             let tempDate: string = tempFullDate.getDate().toString();
                             let tempMonth: string = (tempFullDate.getMonth() + 1).toString();
-
-                            // console.log('seconds', (tempFullDate).getSeconds())
-                            // console.log('minutes', tempFullDate.getMinutes())
-                            // console.log('hours', tempFullDate.getHours())
-                            // console.log('day', tempFullDate.getDate())
-                            // console.log('month', tempFullDate.getMonth())
                             
                             if(tempSeconds.length === 1) {
                                 tempSeconds = `0${tempSeconds}`
@@ -149,50 +108,20 @@ const Graph: React.FC<GraphProps> = ({
                                     break;
                             }
 
-                            console.log('timeLabel', timeLabel)
-                            console.log('timeLabels', tempTimeLabels)
-                            console.log('timeLabelCounts', timeLabelCounts)
-                        
-                            let labelRecorded = false;
-                            let repeatLabel = false;
-
                             if(tempTimeLabels.length === 0) {
-                                console.log('initial push into time leabesl', timeLabelCounts)
                                 tempTimeLabels.push(timeLabel);
-                                timeLabelCounts.push(0);
                             } else {
-                                tempTimeLabels.forEach((label, i) => {
+                                tempTimeLabels.forEach((label) => {
                                     if(label === timeLabel) {
                                         labelRecorded = true;
-                                        // console.log('timeLabelCounts[i]', timeLabelCounts[i])
-                                        // timeLabelCounts[i] = timeLabelCounts[i] + 1;
-                                        // if(timeLabelCounts[i] > 3) {
-                                        //     repeatLabel = true;
-                                        //     timeLabelCounts[i] = 0;
-                                        // }
                                         return;
                                     }
                                 })
+
                                 if(!labelRecorded) {
                                     repeatCount = 0;
-                                    console.log('unique label push', timeLabel)
                                     tempTimeLabels.push(timeLabel);
-                                    // timeLabelCounts.push(0);
-                                    
-                                    console.log('timeLabels', tempTimeLabels)
-                                    console.log('timeLabelCounts', timeLabelCounts)
-                                } 
-                                // else if(repeatLabel) {
-                                //     repeatCount++;
-                                //     console.log('unique label push', timeLabel)
-                                //     tempTimeLabels.push(timeLabel);
-                                    
-                                //     console.log('timeLabels', tempTimeLabels)
-                                //     console.log('timeLabelCounts', timeLabelCounts)
-                                //     repeatLabel = false;
-                                // }
-                                
-                                else {
+                                } else {
                                     repeatCount++;
                                     if(repeatCount > repeatCountLimit) {
                                         tempTimeLabels.push(timeLabel);
@@ -206,40 +135,17 @@ const Graph: React.FC<GraphProps> = ({
                     })
 
                     setTimeLabels(tempTimeLabels);
-
-                    // console.log('FINAL TIME LABELS', tempTimeLabels)
-                    // tempSensorData.forEach((data) => {
-                    //     let labelRecorded = false;
-                    //     if(tempTimeLabels.length === 0) {
-                    //         tempTimeLabels.push(data.timeRecorded);
-                    //     } 
-                        
-                    //     else {
-                    //         tempTimeLabels.forEach((label) => {
-                    //             if(label === data.timeRecorded) {
-                    //                 labelRecorded = true;
-                    //                 return;
-                    //             }
-                    //         })
-                    //         if(!labelRecorded) {
-                    //             tempTimeLabels.push(data.timeRecorded);
-                    //         }
-                    //     }
-                    // })
             }
         }
     }, [isLoggingOut, selectedChannelID, selectedTimeFrame])
 
     useEffect(() => {
         getSensorData(selectedSensorID);
-        console.log('USING THIS EFFECT')
     }, [selectedSensorID, getSensorData, selectedChannelID, selectedTimeFrame]);
 
     return (
         <div className="MainVisualizationBox">
             {
-                // (((selectedSensorID === 0) || selectedSensorID === undefined) ||
-                // ((selectedChannelID === 0) || selectedChannelID === undefined)) ? 
                 (sensorData.length === 0) ? null :
                     <Line
                         data={{
@@ -308,111 +214,8 @@ const Graph: React.FC<GraphProps> = ({
                         }}
                     />
             }
-            
-
-
-            {
-                (((selectedSensorID === 0) || selectedSensorID === undefined) ||
-                    ((selectedChannelID === 0) || selectedChannelID === undefined)) ? 
-                    null :
-                <div className="mainGraphBox">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Sensord Data ID</th>
-                                <th>Data Value</th>
-                                <th>Data Unit</th>
-                                <th>Time Recorded</th>
-                                <th>Sensor ID</th>
-                                <th>Channel ID</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
-                            {
-                                sensorData.map((sensorData, i) => {
-                                    // console.log('sensorData.timeRecorded', sensorData.timeRecorded)
-                                    // console.log('Date.parse(sensorData.timeRecorded)', Date.parse(sensorData.timeRecorded));
-                                    if((sensorData.channelID === selectedChannelID) && 
-                                        ((Date.parse(sensorData.timeRecorded) > (Date.now() - selectedTimeFrame) ||
-                                            (selectedTimeFrame === 0)))
-                                    ) {
-                                        return (
-                                            <tr key={i}>
-                                                <td>{i+1}</td>
-                                                <td>{sensorData.dataValue}</td>
-                                                <td>{sensorData.dataUnit}</td>
-                                                <td>{sensorData.timeRecorded}</td>
-                                                <td>{sensorData.sensorID}</td>
-                                                <td>{sensorData.channelID}</td>
-                                            </tr>
-                                        )    
-                                    } else {
-                                        return null;
-                                    }
-                                })
-                            }
-                        </tbody>
-                    </table>
-
-                    
-                    {/* { 
-                        values ? 
-                        <Line options = {options(values.minValue, values.maxValue, values.dataLabel, values.dataTitle)} data={data(values.values, values.times)}/> : 
-                        null
-                    } */}
-                </div>
-            }
         </div>  
     )
 }
 
 export default Graph;
-
-
-
-                                // sensorData.map((data) => {
-                                //     if(data.channelID === selectedChannelID) {
-                                //         const tempFullDate = new Date(data.timeRecorded);
-                                //         // console.log('tempFullDate', tempFullDate);
-                                //         let tempSeconds: string = tempFullDate.getSeconds().toString();
-                                //         let tempMinutes: string = tempFullDate.getMinutes().toString();
-                                //         let tempHours: string = tempFullDate.getHours().toString();
-                                //         let tempDate: string = tempFullDate.getDate().toString();
-                                //         let tempMonth: string = (tempFullDate.getMonth() + 1).toString();
-                                        
-                                //         if(tempSeconds.length === 1) {
-                                //             tempSeconds = `0${tempSeconds}`
-                                //         }
-                                //         if(tempMinutes.length === 1) {
-                                //             tempMinutes = `0${tempMinutes}`
-                                //         }
-                                //         if(tempHours.length === 1) {
-                                //             tempHours = `0${tempHours}`
-                                //         }
-                                //         if(tempDate.length === 1) {
-                                //             tempDate = `0${tempDate}`
-                                //         }
-                                //         if(tempMonth.length === 1) {
-                                //             tempMonth = `0${tempMonth}`
-                                //         }
-
-                                //         switch(selectedTimeFrame) {
-                                //             case timeFrameConstants.DAY: {
-                                //                 return `${tempHours}:${tempMinutes}:${tempSeconds}`;
-                                //             }
-                                //             case timeFrameConstants.WEEK: 
-                                //             case timeFrameConstants.MONTH: {
-                                //                 return `${tempMonth}/${tempDate}`;
-                                //             }
-                                //             case timeFrameConstants.SIX_MONTHS:
-                                //             case timeFrameConstants.YEAR: {
-                                //                 return `${monthsOfTheYear[tempFullDate.getMonth()]}`;
-                                //             }
-                                //             default:
-                                //                 return `${monthsOfTheYear[tempFullDate.getMonth()]}, ${tempFullDate.getFullYear()}`;
-                                //         }
-                                //     } else {
-                                //         return null;
-                                //     }
-                                // }),
